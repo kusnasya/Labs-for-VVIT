7151cd384b0c5408641dc098ba325932e11727da
Анастасия Безгачева <kusnasya@yandex.ru>
2023-03-19 12:43:45 +0300

    Find the roots of the equation

---
+++ lab1/main.py
@@ -0,0 +1,13 @@
+from math import sqrt
+
+a, b, c = float(input()), float(input()), float(input())
+d = b ** 2 - 4 * a * c
+if d < 0:
+    print('Нет корней')
+elif d == 0:
+    x1 = (-b + sqrt(d)) / (2 * a)
+    print(x1)
+else:
+    x1 = (-b + sqrt(d)) / (2 * a)
+    x2 = (-b - sqrt(d)) / (2 * a)
+    print(min(x1,x2),max(x1,x2), sep='\n')
